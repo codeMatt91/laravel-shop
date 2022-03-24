a426cf651b2d55e50a344f5d092a7a393e501655
AngeloLista <abstractplayer@gmail.com>
2022-03-24 16:11:51 +0100

    added edit function in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -58,9 +58,9 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Product $product)
     {
-        //
+        return view('products.edit', compact('product'));
     }
 
     /**
