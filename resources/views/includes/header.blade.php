9b6a065b7e956172727cfb20f24f44f516b7a8fb
marcotrunc <marcotrunc@gmail.com>
2022-03-24 16:51:17 +0100

    added create page

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -11,9 +11,9 @@
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
-                        <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('products.index') }}">Products</a>
-                </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="{{ route('products.index') }}">Products</a>
+                    </li>
                 </ul>
             </div>
         </div>
